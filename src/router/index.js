dcf6c1914a24cfd44b1bb1f6548b2e75ac682376
hbj <301085344@qq.com>
2017-06-18 19:18:21 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
+let defaultRouters = [{
+	path: '/',
+	component: require('../pages/Toast.vue')
+}];
+
+let routes = Array.prototype.concat.call(defaultRouters);
+
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes
 })
